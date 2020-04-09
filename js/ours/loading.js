788c72f07ed495e84da9ba5276d75d23e1aaeff7
Ana Meisel <aanameisell@gmail.com>
2020-04-09 13:11:14 +0100

    everything in kasia js

--- js/ours/loading.js
+++ js/ours/loading.js
@@ -1,7 +1,1 @@
-window.onload = function(e) {
-	const loader = document.getElementById('loading')
-	loader.style.opacity = '0'
-	setTimeout(function() {
-		loader.parentNode.removeChild(loader)
-	}, 400)
-}
+
